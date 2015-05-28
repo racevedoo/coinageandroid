f1ecccf6823e27f372a38db6fc7e74a68efcf0bb
Rafael Acevedo <raa7@cin.ufpe.br>
2015-05-28 16:08:33 -0300

    Update Application to get DBHelper

--- src/br/ufpe/cin/coinage/android/MainApplication.java
+++ src/br/ufpe/cin/coinage/android/MainApplication.java
@@ -1,14 +1,19 @@
 package br.ufpe.cin.coinage.android;
 
+import br.ufpe.cin.coinage.database.DBHelper;
 import br.ufpe.cin.coinage.network.NetworkQueue;
 import android.app.Application;
 import android.content.Context;
 
 public class MainApplication extends Application {
 	private static Context context;
-	
+	private static DBHelper dbHelper;
 	public static Context getContext(){
 		return context;
+	}
+	
+	public static DBHelper getDBHelper(){
+		return dbHelper;
 	}
 	
 	@Override
@@ -16,6 +21,7 @@ public class MainApplication extends Application {
 		super.onCreate();
 		NetworkQueue.getInstance().init(this);
 		context = getApplicationContext();
+		dbHelper = new DBHelper(context);
 	}
 	
 }
